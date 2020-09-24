650a4a64e281af721e226890f0afd30ade1d13f6
daniel.ach <MrAch26@users.noreply.github.com>
2020-09-24 22:47:48 +0300

    Some IOS dev with Udemy :8ball:

---
+++ I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Daniel Ach on 10/05/2020.
+//  Copyright © 2020 Daniel Ach. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,10 @@
+//this is a comment
+print("Hey \(2+3) Bruuuuu")
+/*jaime les aucisgdgdkjfghdkfhgkdhfgkdhfgkdhfgkdfhgkdhfgkdfhgkdfghdkf
+ dfkjgdkfgjhdkfgjhdkfghd
+ dfjkdhfgkdfghdkhgkdfhgkdfghkdfhgdk*/
+
+var a = 1
+var b = 2
+
+print(a+b)
